c6c9d1179e0de4a5085c0856a5d12f1c86941c4d
dev-acastro <70414513+dev-acastro@users.noreply.github.com>
2021-03-01 19:27:36 -0600

    Where Raw in Reporte

--- app/Http/Controllers/Front/ReporteriaController.php
+++ app/Http/Controllers/Front/ReporteriaController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Reporte;
 use Illuminate\Http\Request;
 use App\Classes\GrupoArray;
 use App\Models\Sector;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class ReporteriaController extends Controller
 {
@@ -17,7 +20,10 @@ class ReporteriaController extends Controller
         $reporteArray = $reporte->getArray();
         $sector = Sector::all()->toArray();
 
+        $reportes = DB::table('reportes')->where("WEEKOFYEAR(`fecha`)", "=", "WEEKOFYEAR(NOW())-1")->get();
+        return count($reportes);
 
-        return view('reportes.reporte', ["reporte"=>$reporteArray, 'sector'=> $sector]);
+
+        return view('reportes.reporte', ["reporte"=>$reporteArray, 'sector'=> $sector, 'reportes' => $reportes]);
     }
 }
--- resources/views/reportes/reporte.blade.php
+++ resources/views/reportes/reporte.blade.php
@@ -4,57 +4,73 @@
 
 @section('content')
 
-    @foreach($sector as $sector)
+{{$reportes}}
 
-        {{array_search("1", $sector)}}
+  {{--  @foreach($sector as $sector)
 
-    @endforeach
+        @if(array_search("Z1D1A1S1", $sector) == "codigo_sector")
+
+
+        {{$sector['supervisor']}}
+        @endif
+
+    @endforeach--}}
+
+    <div class="container">
 
     <h1>Reporte</h1>
 
-
-
-    <table class="table table-striped">
+    <table class="table table-striped table-bordered">
         <thead>
         <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
+            <th>Codigo</th>
+            <th>Grupo</th>
+            <th>Adultos</th>
+            <th>Niños</th>
+            <th>Inconversos</th>
+            <th>Conversiones</th>
+            <th>IBBAJ</th>
+            <th>CCDL</th>
         </tr>
         </thead>
-
         <tbody>
-
-
-
-
-
-
 @foreach($reporte as $x => $zona)
    <tr>
-        <th colspan="3">
-       <div style="width: 50%; background-color: #0B90C4; height: 30px; text-align: center; margin: 10px auto; color:white;">{{$x}}</div>
+        <th colspan="8">
+       <div style="width: 100%; background-color: #0B90C4; height: 30px; text-align: center; margin: 10px auto; color:white;">{{$x}}</div>
         </th>
    </tr>
     @foreach($zona as $y => $distrito)
         <tr>
-            <th colspan="3">
-                <div style="width: 50%; background-color: #00bb00; height: 30px; text-align: center; margin: 10px auto; color:white;">{{$y}}</div>
+            <th colspan="8">
+                <div style="width: 100%; background-color: #00bb00; height: 30px; text-align: center; margin: 10px auto; color:white;">{{$y}}</div>
             </th>
         </tr>
 
             @foreach($distrito as $z => $area)
                 <tr>
-                    <th colspan="3">
-                        <div style="width: 50%; background-color: #2f016b; height: 30px; text-align: center; margin: 10px auto; color:white;">{{$z}}</div>
+                    <th colspan="8">
+                        <div style="width: 100%; background-color: #2f016b; height: 30px; text-align: center; margin: 10px auto; color:white;">{{$z}}</div>
                     </th>
                 </tr>
 
-                @foreach($area as $a => $sector)
+                @foreach($area as $a => $sectores)
                     <tr>
-                        <th>
-                           <!-- <div style="width: 50%; background-color: #8a6d3b; height: 30px; text-align: center; margin: 10px auto; color:white;"></div>-->
-                        </th>
+
+
+                     {{--       <div style="width: 50%; background-color: #8a6d3b; height: 30px; text-align: center; margin: 10px auto; color:white;">--}}
+                                @foreach($sector as $sectorIndividual)
+
+                                    @if(array_search($x.$y.$z.$a, $sectorIndividual) == "codigo_sector")
+
+
+                                <th>  {{$sectorIndividual['codigo_sector']}}</th>
+                                <th>  {{$sectorIndividual['supervisor']}}</th>
+                                    @endif
+
+                                @endforeach
+                            {{--</div>--}}
+
 
                     </tr>
 
@@ -65,5 +81,7 @@
 
         </tbody>
     </table>
+
+    </div>
 
 @endsection
